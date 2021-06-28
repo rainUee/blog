e3914419b38ecd8fbd81902b306098a01945cd9c
Rainee <598180683@qq.com>
2021-06-28 22:33:22 +0800

    test again

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   logo: '/avatar.png',
   favicon: '/avatar.png',
   //base: '/blog/',
-  //publicPath: './',
+  publicPath: '/blog/',
   // more config: https://d.umijs.org/config
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
